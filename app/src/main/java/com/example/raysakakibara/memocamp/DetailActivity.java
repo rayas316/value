43050012b5b559deb9bd4bae2278d670e37e3260
Ray Sakakibara <raysakakibara@RaySakakibara-MacBook-Pro.local>
2018-08-30 17:36:04 +0900

    1

--- app/src/main/java/com/example/raysakakibara/memocamp/DetailActivity.java
+++ app/src/main/java/com/example/raysakakibara/memocamp/DetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.raysakakibara.memocamp;
 
 import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,8 @@ public class DetailActivity extends AppCompatActivity {
     Realm realm;
     TextInputEditText titleText;
     TextInputEditText contentText;
+    TextInputLayout titleEditTextTextInputLayout2;
+    TextInputLayout contentEditTextTextInputLayout2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +43,29 @@ public class DetailActivity extends AppCompatActivity {
 
     public void update(View view) {
         final Memo memo = realm.where(Memo.class).equalTo("updateDate", getIntent().getStringExtra("updateDate")).findFirst();
+        String title=titleText.getText().toString();
+        String content=contentText.getText().toString();
+        titleEditTextTextInputLayout2=findViewById(R.id.titleEditTextTextInputLayout2);
+        contentEditTextTextInputLayout2=findViewById(R.id.contentEditTextTextInputLayout2);
+        if (title.matches("") && content.matches("")) {
 
-
+            contentEditTextTextInputLayout2.setError("商品名と値段が入力されていません");
+            return;
+        }
+        else if (title.matches("")) {
+            titleEditTextTextInputLayout2.setError("商品名が入力されていません");
+            return;
+        }
+        else if (content.matches("")) {
+            contentEditTextTextInputLayout2.setError("値段が入力されていません");
+            return;
+        }else if (title.length()>10){
+            titleEditTextTextInputLayout2.setError("入力できるのは10文字までです");
+            return;
+        }else if (content.length()>10){
+            contentEditTextTextInputLayout2.setError("入力できるのは10桁までです");
+            return;
+        }
         realm.executeTransaction(new Realm.Transaction() {
             @Override
             public void execute(Realm realm) {
